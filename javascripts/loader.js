d3f0cca748e042e5da0567273ba3f781acefee5a
DVD <diogocardoso89@gmail.com>
2019-02-15 17:35:42 +0000

    v1

---
+++ javascripts/loader.js
@@ -0,0 +1,93 @@
+var maxNews = 10;
+
+if (!String.format) {
+  String.format = function(format) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    return format.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number] 
+        : match
+      ;
+    });
+  };
+}
+
+window.onload = function() {
+	loadNews();
+	registerLogHooks();
+ 
+	player = new YT.Player('trailerVideo',
+	{
+		events:
+		{
+            'onStateChange': function(event)
+			{
+				if(event.data == YT.PlayerState.PLAYING)
+				{
+						ga('send', 'event', 'trailer', "play", '');
+				}
+			}
+		}
+	});
+	
+
+};
+
+
+
+function createNews(date, text, link)
+{
+	
+	return String.format("<div><p class=\"newsDate\">{0}/{1}/{2}</p><p class=\"newsText\">{3}</p><p><a href=\"{4}\" class=\"fbLink\">more</a></p></div>",
+		date.getDate(),
+		date.getMonth()+1,
+		date.getFullYear(),
+		text,
+		link
+	);
+	
+}
+
+function loadNews()
+{
+	var fbLink = "https://graph.facebook.com/v2.5/1674449119468400/posts?fields=full_picture,message,created_time,link&access_token=EAAEt1w89jhYBAEprukgLhruo8vwK3CYb6vcjz8FYHjZBgDAIv7S4kZBhRYC3tASu6AZCwPqcAmS9PAUqTTzYdwwpkLkZCgmlY1jmyrWyNvZAyHyXpZBIkZCvrknaJVNHlWbAODaWsdm5tbtZCnTg7C4P2osEv6wa2UrHFrArP9jZBBwZDZD"
+	
+	var maxChars = 200;
+	
+	$.getJSON( fbLink, function( data ) 
+	{
+		var items = [];
+		
+		var arr = data.data;
+		var newsPosts = arr.slice(0,maxNews);
+		
+		$.each( newsPosts, function( key, val ) 
+		{
+			var link = val.link?val.link : "https://www.facebook.com/"+val.id;
+			if(val.message)
+			{
+				var text = val.message.substring(0,maxChars);
+				
+				if(val.message.length > maxChars)
+					text+="...";
+				
+				
+				items.push(createNews(new Date(val.created_time), text, link));
+				/*
+				items.push( "<p class=\"newsDate\">" + val.created_time + "'</p>" + 
+				"<p class=\"newsText\">"+text + "</p>"+
+				"<p><a href=\""+ link   + "\"> more </a> </p>"
+				);
+				*/
+			}
+		});
+	 
+		$( "<div/>", 
+		{
+			"class": "my-new-list",    html: items.join( "" )
+		}).appendTo( "#newsTextContainer" );
+		
+		
+		attachClickEvent(".fbLink", function(elem){return elem.href;}, function(elem){return 'news';});
+	});
+}
---
+++ javascripts/media.js
@@ -0,0 +1,216 @@
+var gutter = 5;
+var placeholderSize = 583;
+var mediaKitLink;
+
+window.onload = function() 
+{
+  loadMedia();
+  registerLogHooks();
+ 
+  
+  
+};
+
+function getUriFor(folderId)
+{
+	return "https://api.onedrive.com/v1.0/drive/items/" + folderId + "/children?authkey=!AHSX3KI1pTAe6BU&orderby=lastModifiedDateTime%20desc";
+}
+
+function getFolderChildren(folderId, callback)
+{
+	$.getJSON(getUriFor(folderId), function(data)
+	{
+		callback(data.value);
+	});
+}
+
+function loadMedia()
+{
+	var rootFolderId = "55963BCCC14A8EC4!219668";
+	var uri = getUriFor(rootFolderId);
+	
+	
+	getFolderChildren(rootFolderId, function(items)
+	{
+
+		items.sort(function(a, b){return b.name.localeCompare(a.name);})
+		
+		$.each(items, function(key,item)
+		{
+			if(!('folder' in item) || item.folder.childCount == 0)	
+				return;
+			
+			var mediaFolder = { name:item.name, id:item.id };
+
+			getFolderImages(mediaFolder)
+		});
+		
+	});
+	
+	
+}
+
+function getFolderImages(folder)
+{
+
+	folder.photos = [];
+	
+	
+	
+	var folderView = $("<div/>", {"class":"contentTitle", "id":folder.name, html:folder.name});
+		
+	folderView.appendTo("#mediaContent");
+	
+	getFolderChildren(folder.id, function(items)
+	{
+		var nrOfImagesPerRow = 2;
+		
+		if(folder.name == "Banners")
+			nrOfImagesPerRow = 3;
+		
+		if(folder.name == "PressKit")
+		{
+			HandleMediaKit(items);
+			folderView.remove();
+			return;
+		}
+		
+		
+		$.each(items, function(key,item)
+		{
+			if(!('image' in item) )	
+				return;
+			
+			var photo = 
+			{ 
+				name:item.name, 
+				id:item.id, 
+				url:item["@content.downloadUrl"],
+				height:item.image.height,
+				width:item.image.width
+			};
+			folder.photos.push(photo);
+		});	
+		
+	
+		
+		publishFolder(folder,folderView,nrOfImagesPerRow);
+	});
+}
+
+function HandleMediaKit(items)
+{
+	var latest = items[0];
+	var link = latest["@content.downloadUrl"];
+	$("#PressKit").attr("href",link + "/" + latest.name);
+	//.click(function()
+	//{
+	//	window.downloadFile(link);
+	//});
+	
+	
+}
+
+function publishFolder(folder, folderView, nrOfImagesPerRow)
+{
+	var content = $("<div/>");
+	
+	var size = (placeholderSize - gutter * (nrOfImagesPerRow-1))/nrOfImagesPerRow;
+	
+	
+	
+	folder.photos.forEach(function(photo)
+	{
+		var img = createImage(photo,folder,true,size);
+		
+		var div = $("<div/>", {"class":folder.name+ " gridImage" });
+		
+		img.appendTo(div);
+		
+		div.appendTo(content);
+	});
+	
+	
+	
+	content.insertAfter(folderView);
+	
+	//
+	content.masonry(
+	{
+		gutter:5,
+		columnWidth: size,
+		fitWidth: true,
+		itemSelector: "."+folder.name,
+		zoom: 
+		{
+			enabled: true,
+		}
+	});
+	
+	
+	content.magnificPopup({
+		delegate: 'a', // child items selector, by clicking on it popup will open
+		type: 'image',
+		gallery: 
+		{
+		  enabled: true
+		},
+		callbacks: {
+			
+			change: function(item) 
+			{
+				var item = this.currItem.img[0].alt;
+				logClick(null,  function(){return item;}, function(){return "mediaViewFull";})
+			}
+  }
+  // other options
+	});
+	
+	//*/
+	attachClickEvent("."+folder.name+".gridImage" , 
+		function(elem)
+		{
+			var pic = $(elem).find("img")[0];
+			return pic.alt;
+		},
+		function(elem)
+		{
+			return 'mediaView';
+		}
+		);
+	//*/
+}
+
+function createImage(photo, folder, resize, imageWidth)
+{
+	var height = photo.height;
+	var width = photo.width;
+	
+	if(resize && photo.width > imageWidth)
+	{
+		height/=photo.width /imageWidth ;
+		width = imageWidth;
+	}
+		
+	var img  = $('<img />', 
+	{ 
+		src: photo.url,
+		alt: photo.name,
+		"height":height,
+		"width":width
+	});
+	
+	
+	var fullScreenImg = $("<a/>", {"href":photo.url});
+	
+	img.appendTo(fullScreenImg);
+	/*/
+	fullScreenImg.magnificPopup(
+	{
+		type: 'image',
+		closeOnContentClick:true,
+		
+	});
+	//*/
+	return fullScreenImg;
+}
